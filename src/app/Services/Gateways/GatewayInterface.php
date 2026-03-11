1820e621de6809a16774f33a3eae12ffb0c7b113
carloshipol <carlaogaruda30@gmail.com>
2026-03-11 20:16:45 -0300

    feat: add gateway interface

---
+++ src/app/Services/Gateways/GatewayInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\Gateways;
+
+interface GatewayInterface
+{
+    public function charge(array $data): array;
+
+    public function refund(string $transactionId): bool;
+}
