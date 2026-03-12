c273ab2533398c89ee941eb3a33ec8207acf9eae
carloshipol <carlaogaruda30@gmail.com>
2026-03-11 21:50:33 -0300

    feat: add authentication and purchase routes

---
+++ src/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PurchaseController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/purchase', [PurchaseController::class, 'store']);
